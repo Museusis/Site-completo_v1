2a6670264fb87b8352094e2b88ba7689febe57c3
PauloCafasso <paulo.cafasso@sptech.school>
2023-05-08 14:40:15 -0300

    Conexão com a API da Julia substituindo o antigo, adicionar div alerta Login

---
+++ Site/src/models/usuarioModel.js
@@ -0,0 +1,38 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function entrar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(nome, sobrenome, CPF, email, dtnasc, cidade, CEP, senha, confirmaSenha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",nome, sobrenome, dtnasc, CPF, cidade, CEP, email, senha, confirmaSenha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO usuario (nome, sobrenome, cpf, email, dtNasc, cidade, CEP, senha, confirmaSenha) VALUES ('${nome}', '${sobrenome}', '${CPF}', '${email}', '${dtnasc}', '${cidade}', '${CEP}','${senha}','${confirmaSenha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+};
